ca32a4476ff12796316da84fd669a4fcd21c32bc
Killingova <ms.zhivkova@googlemail.com>
2024-04-04 18:52:04 +0200

    Ende

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,20 @@
             CmdStopX = new Button();
             LblAnzeige = new Label();
             TimAnzeige = new System.Windows.Forms.Timer(components);
+            PanelBlau = new Panel();
+            PanelGelb = new Panel();
+            PanelRot = new Panel();
+            PanelGruen = new Panel();
+            CmdLetsPlay = new Button();
+            TimPlay = new System.Windows.Forms.Timer(components);
+            CmdLetsPause = new Button();
             SuspendLayout();
             // 
             // CmdStartX
             // 
-            CmdStartX.Location = new Point(52, 303);
+            CmdStartX.Location = new Point(333, 768);
             CmdStartX.Name = "CmdStartX";
-            CmdStartX.Size = new Size(94, 29);
+            CmdStartX.Size = new Size(95, 53);
             CmdStartX.TabIndex = 0;
             CmdStartX.Text = "Start";
             CmdStartX.UseVisualStyleBackColor = true;
@@ -47,9 +54,9 @@
             // 
             // CmdStopX
             // 
-            CmdStopX.Location = new Point(52, 358);
+            CmdStopX.Location = new Point(445, 768);
             CmdStopX.Name = "CmdStopX";
-            CmdStopX.Size = new Size(94, 29);
+            CmdStopX.Size = new Size(95, 53);
             CmdStopX.TabIndex = 1;
             CmdStopX.Text = "Stop";
             CmdStopX.UseVisualStyleBackColor = true;
@@ -59,7 +66,7 @@
             // 
             LblAnzeige.AutoSize = true;
             LblAnzeige.BorderStyle = BorderStyle.FixedSingle;
-            LblAnzeige.Location = new Point(64, 155);
+            LblAnzeige.Location = new Point(573, 768);
             LblAnzeige.Name = "LblAnzeige";
             LblAnzeige.Size = new Size(2, 22);
             LblAnzeige.TabIndex = 2;
@@ -69,16 +76,84 @@
             TimAnzeige.Interval = 500;
             TimAnzeige.Tick += TimAnzeige_Tick;
             // 
+            // PanelBlau
+            // 
+            PanelBlau.BackColor = Color.FromArgb(192, 255, 255);
+            PanelBlau.Location = new Point(410, 324);
+            PanelBlau.Name = "PanelBlau";
+            PanelBlau.Size = new Size(30, 30);
+            PanelBlau.TabIndex = 4;
+            PanelBlau.Paint += PanelBlau_Paint;
+            // 
+            // PanelGelb
+            // 
+            PanelGelb.BackColor = Color.FromArgb(255, 224, 192);
+            PanelGelb.Location = new Point(312, 423);
+            PanelGelb.Name = "PanelGelb";
+            PanelGelb.Size = new Size(30, 30);
+            PanelGelb.TabIndex = 5;
+            PanelGelb.Paint += PanelGelb_Paint;
+            // 
+            // PanelRot
+            // 
+            PanelRot.BackColor = Color.FromArgb(255, 192, 192);
+            PanelRot.Location = new Point(312, 324);
+            PanelRot.Name = "PanelRot";
+            PanelRot.Size = new Size(30, 30);
+            PanelRot.TabIndex = 6;
+            PanelRot.Paint += PanelRot_Paint;
+            // 
+            // PanelGruen
+            // 
+            PanelGruen.BackColor = Color.FromArgb(192, 255, 192);
+            PanelGruen.Location = new Point(410, 423);
+            PanelGruen.Name = "PanelGruen";
+            PanelGruen.Size = new Size(30, 30);
+            PanelGruen.TabIndex = 7;
+            PanelGruen.Paint += PanelGruen_Paint;
+            // 
+            // CmdLetsPlay
+            // 
+            CmdLetsPlay.Location = new Point(205, 768);
+            CmdLetsPlay.Name = "CmdLetsPlay";
+            CmdLetsPlay.Size = new Size(95, 53);
+            CmdLetsPlay.TabIndex = 8;
+            CmdLetsPlay.Text = "Play";
+            CmdLetsPlay.UseVisualStyleBackColor = true;
+            CmdLetsPlay.Click += CmdLetsPlay_Click;
+            // 
+            // TimPlay
+            // 
+            TimPlay.Interval = 1000;
+            TimPlay.Tick += TimPlay_Tick;
+            // 
+            // CmdLetsPause
+            // 
+            CmdLetsPause.Location = new Point(70, 768);
+            CmdLetsPause.Name = "CmdLetsPause";
+            CmdLetsPause.Size = new Size(95, 53);
+            CmdLetsPause.TabIndex = 9;
+            CmdLetsPause.Text = "Pause";
+            CmdLetsPause.UseVisualStyleBackColor = true;
+            CmdLetsPause.Click += CmdLetsPause_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 853);
+            Controls.Add(CmdLetsPause);
+            Controls.Add(CmdLetsPlay);
+            Controls.Add(PanelGruen);
+            Controls.Add(PanelRot);
+            Controls.Add(PanelGelb);
+            Controls.Add(PanelBlau);
             Controls.Add(LblAnzeige);
             Controls.Add(CmdStopX);
             Controls.Add(CmdStartX);
             Name = "Form1";
             Text = "SteuerelementTimer";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -89,5 +164,12 @@
         private Button CmdStopX;
         private Label LblAnzeige;
         private System.Windows.Forms.Timer TimAnzeige;
+        private Panel PanelBlau;
+        private Panel PanelGelb;
+        private Panel PanelRot;
+        private Panel PanelGruen;
+        private Button CmdLetsPlay;
+        private System.Windows.Forms.Timer TimPlay;
+        private Button CmdLetsPause;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,69 @@ namespace TimerTick
 
         private void CmdStopX_Click(object sender, EventArgs e)
         {
-            TimAnzeige.Enabled=false;
+            TimAnzeige.Enabled = false;
         }
 
         private void TimAnzeige_Tick(object sender, EventArgs e)
         {
             LblAnzeige.Text += "x";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PanelRot_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PanelBlau_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PanelGelb_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PanelGruen_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CmdLetsPlay_Click(object sender, EventArgs e)
+        {
+            TimPlay.Enabled = true;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TimPlay_Tick(object sender, EventArgs e)
+        {
+
+            // Bewegt das rote Panel diagonal nach oben links.
+            PanelRot.Location = new Point(PanelRot.Location.X - 10, PanelRot.Location.Y - 10);
+
+            // Bewegt das blaue Panel diagonal nach oben rechts.
+            PanelBlau.Location = new Point(PanelBlau.Location.X + 10, PanelBlau.Location.Y - 10);
+
+            // Bewegt das gelbe Panel diagonal nach unten links.
+            PanelGelb.Location = new Point(PanelGelb.Location.X - 10, PanelGelb.Location.Y + 10);
+
+            // Bewegt das grüne Panel diagonal nach unten rechts.
+            PanelGruen.Location = new Point(PanelGruen.Location.X + 10, PanelGruen.Location.Y + 10);
+        }
+
+        private void CmdLetsPause_Click(object sender, EventArgs e)
+        {
+            TimPlay.Enabled = false;
         }
     }
 }
